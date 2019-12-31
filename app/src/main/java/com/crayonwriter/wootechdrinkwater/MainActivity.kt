30f95f07898130db4c40ba4b1f61f4c298b481f9
Theda Rogers <crayonwriter@gmail.com>
2019-12-30 20:04:28 -0500

    First commit

---
+++ app/src/main/java/com/crayonwriter/wootechdrinkwater/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.crayonwriter.wootechdrinkwater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
