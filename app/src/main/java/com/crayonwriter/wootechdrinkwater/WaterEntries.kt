d3bdebecac33ca1681fb77141d6eb35182ac3b26
Theda Rogers <crayonwriter@gmail.com>
2020-01-08 19:02:31 -0500

    Add primary key and 2 columns to WaterEntries data class

--- app/src/main/java/com/crayonwriter/wootechdrinkwater/WaterEntries.kt
+++ app/src/main/java/com/crayonwriter/wootechdrinkwater/WaterEntries.kt
@@ -1,4 +1,17 @@
 package com.crayonwriter.wootechdrinkwater
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity
+data class waterEntries(
+    @PrimaryKey
+    @ColumnInfo(name = "date")
+    val date: String = drinkDate.toString(),
+    @ColumnInfo(name = "water_glasses")
+    val waterGlasses: String
+)
+
+
 
