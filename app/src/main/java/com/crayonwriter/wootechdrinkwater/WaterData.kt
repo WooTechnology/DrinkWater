0bc231fc819619e2520ddb5abce0de4828efd50a
Theda Rogers <crayonwriter@gmail.com>
2020-01-09 23:30:18 -0500

    Create instance of database

--- app/src/main/java/com/crayonwriter/wootechdrinkwater/WaterData.kt
+++ app/src/main/java/com/crayonwriter/wootechdrinkwater/WaterData.kt
@@ -1,9 +1,16 @@
 package com.crayonwriter.wootechdrinkwater
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(WaterEntries::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): WaterDao
 }
+
+//TODO: Use correct context and create database instance in correct location
+val db = Room.databaseBuilder(
+    applicationContext,
+    AppDatabase::class.java, "database-name"
+).build()
